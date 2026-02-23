c329a197ab3cd2ac95dbdcea2fc62eae14a5b5b1
Maku4444 <dzulaymakar@gmail.com>
2026-02-23 15:19:37 +0200

    Update zvd1.c

--- pr2/zvd1/zvd1.c
+++ pr2/zvd1/zvd1.c
@@ -3,15 +3,14 @@
 #include <limits.h>
 
 int main() {
-    printf("Дослідження розрядності time_t:\n");
-    printf("Розмір типу на вашій архітектурі: %zu байт\n", sizeof(time_t));
+    printf("Ваша поточна архітектура: %zu-bit (time_t = %zu байт)\n\n", sizeof(time_t) * 8, sizeof(time_t));
 
-    if (sizeof(time_t) == 4) {
-        time_t max_32 = (time_t)INT_MAX;
-        printf("Межа (32-bit): %s", ctime(&max_32));
-    } else {
-        time_t max_64 = (time_t)LLONG_MAX;
-        printf("Межа (64-bit): %s", ctime(&max_64));
+    time_t max_32 = (time_t)2147483647;
+    printf("ЛІМІТ ДЛЯ 32-BIT (Проблема 2038): %s", ctime(&max_32));
+
+    if (sizeof(time_t) == 8) {
+        time_t max_64 = (time_t)9223372036854775807LL; 
+        printf("ЛІМІТ ДЛЯ 64-BIT:                 %s", ctime(&max_64));
     }
 
     return 0;
